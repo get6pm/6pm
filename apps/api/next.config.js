4484a8a7019ee3e504bcda8d48a825daef8f78c5
Dustin Do <dustin.do95@gmail.com>
2024-06-09 05:03:06 +0700

    feat(api): set base path to /v1 (#52)

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: () => [
+    { source: '/v1/:path*', destination: '/api/v1/:path*' },
+  ]
 }
 
 module.exports = nextConfig

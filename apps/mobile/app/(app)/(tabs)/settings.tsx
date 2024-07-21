dce79e034a0ba9a0a47d018d6fee8c50d9fea0e9
Quốc Khánh <bkdev98@gmail.com>
2024-07-21 20:22:15 +0700

    feat(mobile): add interactive profile card

--- apps/mobile/app/(app)/(tabs)/settings.tsx
+++ apps/mobile/app/(app)/(tabs)/settings.tsx
@@ -2,16 +2,18 @@ import * as Application from 'expo-application'
 
 import { Logo } from '@/components/common/logo'
 import { MenuItem } from '@/components/common/menu-item'
-import { UserAvatar } from '@/components/common/user-avatar'
+import { ProfileCard } from '@/components/setting/profile-card'
 import { SelectDefaultCurrency } from '@/components/setting/select-default-currency'
-import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
 import { Switch } from '@/components/ui/switch'
 import { Text } from '@/components/ui/text'
+import { useColorScheme } from '@/hooks/useColorScheme'
+import { theme } from '@/lib/theme'
 import { useLocale } from '@/locales/provider'
-import { useAuth, useUser } from '@clerk/clerk-expo'
+import { useAuth } from '@clerk/clerk-expo'
 import { t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
+import { LinearGradient } from 'expo-linear-gradient'
 import { Link } from 'expo-router'
 import {
   BellIcon,
@@ -22,7 +24,6 @@ import {
   LockKeyholeIcon,
   LogOutIcon,
   MessageSquareQuoteIcon,
-  PencilIcon,
   ScanFaceIcon,
   ShapesIcon,
   Share2Icon,
@@ -34,196 +35,197 @@ import { Alert, Linking, ScrollView, View } from 'react-native'
 
 export default function SettingsScreen() {
   const { signOut } = useAuth()
-  const { user } = useUser()
   const { i18n } = useLingui()
   const { language } = useLocale()
+  const { colorScheme } = useColorScheme()
 
   return (
-    <ScrollView contentContainerClassName="py-4 gap-4" className="bg-card">
-      <View className="bg-muted rounded-lg overflow-hidden mx-6 justify-end h-44">
-        <View className="flex flex-1" />
-        <View className="flex flex-row px-4 py-3 items-center gap-2 justify-between">
-          <View className="flex flex-row items-center gap-3">
-            <UserAvatar user={user!} fallbackClassName="bg-card" />
-            <View>
-              <Badge variant="outline" className="self-start rounded-md mb-1">
-                <Text className="text-xs font-medium">Free</Text>
-              </Badge>
-              <Text className="font-medium text-primary">
-                {user?.fullName ?? user?.primaryEmailAddress?.emailAddress}
-              </Text>
-            </View>
+    <View className="bg-card">
+      <ScrollView
+        contentContainerClassName="py-4 pb-14 gap-4"
+        className="bg-card"
+      >
+        <ProfileCard />
+        <Button className="mx-6 !px-4 !h-14 justify-between">
+          <View>
+            <Text className="font-semibold !text-base">
+              {t(i18n)`Get 6pm Pro`}
+            </Text>
+            <Text className="font-medium opacity-65 !text-xs">
+              {t(i18n)`Unlocks full AI power and more!`}
+            </Text>
           </View>
-          <Button size="icon" variant="ghost">
-            <PencilIcon className="w-5 h-5 text-primary" />
-          </Button>
-        </View>
-      </View>
-      <Button className="mx-6 !px-4 !h-14 justify-between">
-        <View>
-          <Text className="font-semibold !text-base">
-            {t(i18n)`Get 6pm Pro`}
+          <LockKeyholeIcon className="w-6 h-6 text-muted-foreground" />
+        </Button>
+        <View className="gap-2 mt-4">
+          <Text className="font-sans mx-6 text-muted-foreground">
+            {t(i18n)`General`}
           </Text>
-          <Text className="font-medium opacity-65 !text-xs">
-            {t(i18n)`Unlocks full AI power and more!`}
-          </Text>
-        </View>
-        <LockKeyholeIcon className="w-6 h-6 text-muted-foreground" />
-      </Button>
-      <View className="gap-2 mt-4">
-        <Text className="font-sans mx-6 text-muted-foreground">
-          {t(i18n)`General`}
-        </Text>
-        <View>
-          <Link href="/wallet/accounts" asChild>
-            <MenuItem
-              label={t(i18n)`Wallet accounts`}
-              icon={WalletCardsIcon}
-              rightSection={
-                <ChevronRightIcon className="w-5 h-5 text-primary" />
-              }
-            />
-          </Link>
-          <Link href="/category" asChild>
-            <MenuItem
-              label={t(i18n)`Categories`}
-              icon={ShapesIcon}
-              rightSection={
-                <ChevronRightIcon className="w-5 h-5 text-primary" />
-              }
-            />
-          </Link>
-          <Link href="/magic-inbox" asChild disabled>
-            <MenuItem
-              label={t(i18n)`Magic inbox`}
-              icon={InboxIcon}
-              rightSection={
-                <ChevronRightIcon className="w-5 h-5 text-primary" />
-              }
-            />
-          </Link>
-        </View>
-      </View>
-      <View className="gap-2">
-        <Text className="font-sans mx-6 text-muted-foreground">
-          {t(i18n)`App settings`}
-        </Text>
-        <View>
-          <Link href="/appearance" asChild>
-            <MenuItem
-              label={t(i18n)`Appearance`}
-              icon={SwatchBookIcon}
-              rightSection={
-                <ChevronRightIcon className="w-5 h-5 text-primary" />
-              }
-            />
-          </Link>
-          <Link href="/language" asChild>
-            <MenuItem
-              label={t(i18n)`Language`}
-              icon={EarthIcon}
-              rightSection={
-                <View className="flex flex-row items-center gap-2">
-                  <Text className="text-muted-foreground font-sans uppercase">
-                    {t(i18n)`${language}`}
-                  </Text>
+          <View>
+            <Link href="/wallet/accounts" asChild>
+              <MenuItem
+                label={t(i18n)`Wallet accounts`}
+                icon={WalletCardsIcon}
+                rightSection={
                   <ChevronRightIcon className="w-5 h-5 text-primary" />
-                </View>
+                }
+              />
+            </Link>
+            <Link href="/category" asChild>
+              <MenuItem
+                label={t(i18n)`Categories`}
+                icon={ShapesIcon}
+                rightSection={
+                  <ChevronRightIcon className="w-5 h-5 text-primary" />
+                }
+              />
+            </Link>
+            <Link href="/magic-inbox" asChild disabled>
+              <MenuItem
+                label={t(i18n)`Magic inbox`}
+                icon={InboxIcon}
+                rightSection={
+                  <ChevronRightIcon className="w-5 h-5 text-primary" />
+                }
+              />
+            </Link>
+          </View>
+        </View>
+        <View className="gap-2">
+          <Text className="font-sans mx-6 text-muted-foreground">
+            {t(i18n)`App settings`}
+          </Text>
+          <View>
+            <Link href="/appearance" asChild>
+              <MenuItem
+                label={t(i18n)`Appearance`}
+                icon={SwatchBookIcon}
+                rightSection={
+                  <ChevronRightIcon className="w-5 h-5 text-primary" />
+                }
+              />
+            </Link>
+            <Link href="/language" asChild>
+              <MenuItem
+                label={t(i18n)`Language`}
+                icon={EarthIcon}
+                rightSection={
+                  <View className="flex flex-row items-center gap-2">
+                    <Text className="text-muted-foreground font-sans uppercase">
+                      {t(i18n)`${language}`}
+                    </Text>
+                    <ChevronRightIcon className="w-5 h-5 text-primary" />
+                  </View>
+                }
+              />
+            </Link>
+            <SelectDefaultCurrency />
+            <MenuItem
+              label={t(i18n)`Login using FaceID`}
+              icon={ScanFaceIcon}
+              rightSection={
+                <Switch checked={false} onCheckedChange={console.log} />
               }
             />
-          </Link>
-          <SelectDefaultCurrency />
-          <MenuItem
-            label={t(i18n)`Login using FaceID`}
-            icon={ScanFaceIcon}
-            rightSection={
-              <Switch checked={false} onCheckedChange={console.log} />
-            }
-          />
-          <MenuItem
-            label={t(i18n)`Push notifications`}
-            icon={BellIcon}
-            rightSection={
-              <Switch checked={false} onCheckedChange={console.log} />
-            }
-          />
-        </View>
-      </View>
-      <View className="gap-2">
-        <Text className="font-sans mx-6 text-muted-foreground">
-          {t(i18n)`Others`}
-        </Text>
-        <View>
-          <Link href="/feedback" asChild disabled>
             <MenuItem
-              label={t(i18n)`Send feedback`}
-              icon={MessageSquareQuoteIcon}
+              label={t(i18n)`Push notifications`}
+              icon={BellIcon}
+              rightSection={
+                <Switch checked={false} onCheckedChange={console.log} />
+              }
+            />
+          </View>
+        </View>
+        <View className="gap-2">
+          <Text className="font-sans mx-6 text-muted-foreground">
+            {t(i18n)`Others`}
+          </Text>
+          <View>
+            <Link href="/feedback" asChild disabled>
+              <MenuItem
+                label={t(i18n)`Send feedback`}
+                icon={MessageSquareQuoteIcon}
+                rightSection={
+                  <ChevronRightIcon className="w-5 h-5 text-primary" />
+                }
+              />
+            </Link>
+            <MenuItem
+              label={t(i18n)`Rate 6pm on App Store`}
+              icon={StarIcon}
               rightSection={
                 <ChevronRightIcon className="w-5 h-5 text-primary" />
               }
+              disabled
             />
-          </Link>
-          <MenuItem
-            label={t(i18n)`Rate 6pm on App Store`}
-            icon={StarIcon}
-            rightSection={<ChevronRightIcon className="w-5 h-5 text-primary" />}
-            disabled
-          />
-          <MenuItem
-            label={t(i18n)`Share with friends`}
-            icon={Share2Icon}
-            rightSection={<ChevronRightIcon className="w-5 h-5 text-primary" />}
-            disabled
-          />
-          <MenuItem
-            label={t(i18n)`Proudly open source`}
-            icon={GithubIcon}
-            rightSection={<ChevronRightIcon className="w-5 h-5 text-primary" />}
-            onPress={() => Linking.openURL('https://github.com/sixpm-ai/6pm')}
-          />
-          <Button
-            variant="ghost"
-            onPress={() =>
-              Alert.alert(t(i18n)`Are you sure you want to sign out?`, '', [
-                {
-                  text: t(i18n)`Cancel`,
-                  style: 'cancel',
-                },
-                {
-                  text: t(i18n)`Sign out`,
-                  style: 'destructive',
-                  onPress: () => signOut(),
-                },
-              ])
-            }
-            className="justify-start gap-6 !px-6"
-          >
-            <LogOutIcon className="w-5 h-5 text-red-500" />
-            <Text className="text-red-500 font-regular group-active:text-red-500">
-              {t(i18n)`Sign out`}
-            </Text>
-          </Button>
+            <MenuItem
+              label={t(i18n)`Share with friends`}
+              icon={Share2Icon}
+              rightSection={
+                <ChevronRightIcon className="w-5 h-5 text-primary" />
+              }
+              disabled
+            />
+            <MenuItem
+              label={t(i18n)`Proudly open source`}
+              icon={GithubIcon}
+              rightSection={
+                <ChevronRightIcon className="w-5 h-5 text-primary" />
+              }
+              onPress={() => Linking.openURL('https://github.com/sixpm-ai/6pm')}
+            />
+            <Button
+              variant="ghost"
+              onPress={() =>
+                Alert.alert(t(i18n)`Are you sure you want to sign out?`, '', [
+                  {
+                    text: t(i18n)`Cancel`,
+                    style: 'cancel',
+                  },
+                  {
+                    text: t(i18n)`Sign out`,
+                    style: 'destructive',
+                    onPress: () => signOut(),
+                  },
+                ])
+              }
+              className="justify-start gap-6 !px-6"
+            >
+              <LogOutIcon className="w-5 h-5 text-red-500" />
+              <Text className="text-red-500 font-regular group-active:text-red-500">
+                {t(i18n)`Sign out`}
+              </Text>
+            </Button>
+          </View>
         </View>
-      </View>
-      <View className="items-center gap-3">
-        <Logo className="w-16 h-16 mx-auto" />
-        <Text className="font-medium text-muted-foreground text-sm">
-          {t(i18n)`ver.`}
-          {Application.nativeApplicationVersion}
-        </Text>
-        <View className="flex-row gap-6">
-          <Link href="/terms-of-service">
-            <Text className="text-muted-foreground text-sm">
-              {t(i18n)`Terms of use`}
-            </Text>
-          </Link>
-          <Link href="/privacy-policy">
-            <Text className="text-muted-foreground text-sm">
-              {t(i18n)`Privacy policy`}
-            </Text>
-          </Link>
+        <View className="items-center gap-3">
+          <Logo className="w-16 h-16 mx-auto" />
+          <Text className="font-medium text-muted-foreground text-sm">
+            {t(i18n)`ver.`}
+            {Application.nativeApplicationVersion}
+          </Text>
+          <View className="flex-row gap-6">
+            <Link href="/terms-of-service">
+              <Text className="text-muted-foreground text-sm">
+                {t(i18n)`Terms of use`}
+              </Text>
+            </Link>
+            <Link href="/privacy-policy">
+              <Text className="text-muted-foreground text-sm">
+                {t(i18n)`Privacy policy`}
+              </Text>
+            </Link>
+          </View>
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+      <LinearGradient
+        colors={[
+          colorScheme === 'dark' ? 'transparent' : '#ffffff00',
+          theme[colorScheme ?? 'light'].background,
+        ]}
+        className="absolute bottom-0 left-0 right-0 h-20"
+        pointerEvents="none"
+      />
+    </View>
   )
 }
---
+++ apps/mobile/components/setting/profile-card.tsx
@@ -0,0 +1,168 @@
+import { useUser } from '@clerk/clerk-expo'
+import { BlurView } from 'expo-blur'
+import { PencilIcon } from 'lucide-react-native'
+import { Dimensions, Pressable, View } from 'react-native'
+
+import { UserAvatar } from '../common/user-avatar'
+import { Badge } from '../ui/badge'
+import { Button } from '../ui/button'
+import { Text } from '../ui/text'
+
+import Animated, {
+  type SharedValue,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withDelay,
+  withSequence,
+  withTiming,
+} from 'react-native-reanimated'
+
+const COLS = 15
+const ROWS = 15
+const { width } = Dimensions.get('window')
+const circleBoxSize = width / COLS + 2
+const dots = [...Array(ROWS).keys()].map((rowIndex) =>
+  [...Array(COLS).keys()].map((colIndex) => ({
+    key: rowIndex * COLS + colIndex,
+    row: rowIndex,
+    col: colIndex,
+  })),
+)
+
+enum Distance {
+  Manhattan = 'Manhattan',
+  Euclidian = 'Euclidian',
+  Chebyshev = 'Chebyshev',
+}
+
+function distanceAlgo(
+  distance: Distance,
+  X1: number = 0,
+  Y1: number = 0,
+  X2: number = 0,
+  Y2: number = 0,
+) {
+  'worklet'
+  const distanceX = X2 - X1
+  const distanceY = Y2 - Y1
+  if (distance === Distance.Manhattan) {
+    return Math.abs(X1 - X2) + Math.abs(Y1 - Y2)
+  }
+  if (distance === Distance.Euclidian) {
+    return Math.sqrt(distanceX * distanceX + distanceY * distanceY)
+  }
+
+  if (distance === Distance.Chebyshev) {
+    return Math.max(Math.abs(X1 - X2), Math.abs(Y1 - Y2))
+  }
+}
+const staggerDelay = 60
+type DotProps = {
+  dot: (typeof dots)[0][0]
+  fromIndex: SharedValue<(typeof dots)[0][0]>
+}
+
+const Dot = ({ dot, fromIndex }: DotProps) => {
+  const distance = useDerivedValue(() => {
+    return (
+      (distanceAlgo(
+        Distance.Manhattan,
+        fromIndex.value.col,
+        fromIndex.value.row,
+        dot.col,
+        dot.row,
+      ) || 0) * staggerDelay
+    )
+  })
+
+  const dotStyle = useAnimatedStyle(() => {
+    const scale = withDelay(
+      distance.value,
+      withSequence(
+        withTiming(1, { duration: staggerDelay * 5 }),
+        withTiming(0.3, { duration: staggerDelay * 3 }),
+      ),
+    )
+    const color = withDelay(
+      distance.value,
+      withSequence(
+        withTiming(1.0, { duration: staggerDelay * 3 }),
+        withTiming(0.2, { duration: staggerDelay * 3 }),
+      ),
+    )
+    return {
+      opacity: color,
+      transform: [
+        {
+          scale: scale,
+        },
+      ],
+    }
+  })
+
+  return (
+    <Animated.View
+      style={[
+        {
+          margin: circleBoxSize / 2 - 5,
+        },
+        dotStyle,
+      ]}
+      className="w-1.5 h-1.5 rounded-full bg-muted-foreground"
+      removeClippedSubviews
+      renderToHardwareTextureAndroid
+    />
+  )
+}
+
+export function ProfileCard() {
+  const { user } = useUser()
+  const fromIndex = useSharedValue(
+    dots[Math.round(ROWS / 2)][Math.round(COLS / 2)],
+  )
+
+  return (
+    <View className="bg-muted rounded-lg overflow-hidden mx-6 justify-end h-52">
+      <View className="flex flex-1">
+        {dots.map((row, rowIndex) => {
+          return (
+            <View className="flex-row" key={rowIndex.toString()}>
+              {row.map((dot) => {
+                return (
+                  <Pressable
+                    key={dot.key.toString()}
+                    onPress={() => {
+                      fromIndex.value = dot
+                    }}
+                  >
+                    <Dot dot={dot} fromIndex={fromIndex} />
+                  </Pressable>
+                )
+              })}
+            </View>
+          )
+        })}
+      </View>
+      <BlurView
+        intensity={15}
+        className="flex flex-row px-4 py-3 items-center gap-2 justify-between"
+      >
+        <View className="flex flex-row items-center gap-3">
+          <UserAvatar user={user!} fallbackClassName="bg-card" />
+          <View>
+            <Badge variant="default" className="self-start rounded-md mb-1">
+              <Text className="text-xs font-medium">Saver</Text>
+            </Badge>
+            <Text className="font-medium text-primary">
+              {user?.fullName ?? user?.primaryEmailAddress?.emailAddress}
+            </Text>
+          </View>
+        </View>
+        <Button size="icon" variant="ghost">
+          <PencilIcon className="w-5 h-5 text-primary" />
+        </Button>
+      </BlurView>
+    </View>
+  )
+}

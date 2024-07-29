f776577a8bf49e489e6980f343ec97df376e3970
Quốc Khánh <bkdev98@gmail.com>
2024-07-30 00:39:55 +0700

    feat(mobile): integrate sentry for crashlytics

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,30 +1,26 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+const { withNativeWind } = require('nativewind/metro')
+const { getSentryExpoConfig } = require('@sentry/react-native/metro')
 
-const path = require("path");
+const path = require('path')
 
 // Find the project and workspace directories
-const projectRoot = __dirname;
+const projectRoot = __dirname
 // This can be replaced with `find-yarn-workspace-root`
-const monorepoRoot = path.resolve(projectRoot, "../..");
+const monorepoRoot = path.resolve(projectRoot, '../..')
 
-const config = getDefaultConfig(projectRoot);
+const config = getSentryExpoConfig(projectRoot)
 
 // 1. Watch all files within the monorepo
-config.watchFolders = [monorepoRoot];
+config.watchFolders = [monorepoRoot]
 // 2. Let Metro know where to resolve packages and in what order
 config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, "node_modules"),
-  path.resolve(monorepoRoot, "node_modules"),
-];
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(monorepoRoot, 'node_modules'),
+]
 
-config.resolver.unstable_conditionNames = [
-  "browser",
-  "require",
-  "react-native",
-];
+config.resolver.unstable_conditionNames = ['browser', 'require', 'react-native']
 
-config.resolver.unstable_enableSymlinks = true;
-config.resolver.unstable_enablePackageExports = true;
+config.resolver.unstable_enableSymlinks = true
+config.resolver.unstable_enablePackageExports = true
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+module.exports = withNativeWind(config, { input: './global.css' })

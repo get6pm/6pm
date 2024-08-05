253f61c0523f35d838df17927055bbf7b766ce40
Dustin Do <dustin.do95@gmail.com>
2024-08-05 23:50:54 +0700

    feat(mobile): use transactions in store to calculate wallet stats

--- apps/mobile/components/home/wallet-statistics.tsx
+++ apps/mobile/components/home/wallet-statistics.tsx
@@ -1,23 +1,19 @@
-import { useWallets } from '@/queries/wallet'
+import { useTransactionStore } from '@/stores/transaction/store'
 import { t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
 import { Pressable, View } from 'react-native'
 import { AmountFormat } from '../common/amount-format'
-import { Skeleton } from '../ui/skeleton'
 import { Text } from '../ui/text'
 
 export function WalletStatistics() {
   const { i18n } = useLingui()
-  const { data: walletAccounts, isLoading } = useWallets()
+  const { transactions } = useTransactionStore()
 
   /**
    * TODO: Calculate correct amount with currency exchange rate
    * base on the user's preferred currency
    */
-  const currentBalance = walletAccounts?.reduce(
-    (acc, walletAccount) => acc + (walletAccount?.balance ?? 0),
-    0,
-  )
+  const currentBalance = transactions.reduce((acc, t) => acc + t.amount, 0)
 
   return (
     <View className="gap-3">
@@ -26,11 +22,7 @@ export function WalletStatistics() {
           {t(i18n)`Current balance`}
         </Text>
       </Pressable>
-      {isLoading ? (
-        <Skeleton className="h-10 w-44" />
-      ) : (
-        <AmountFormat amount={currentBalance} size="xl" displayNegativeSign />
-      )}
+      <AmountFormat amount={currentBalance} size="xl" displayNegativeSign />
     </View>
   )
 }

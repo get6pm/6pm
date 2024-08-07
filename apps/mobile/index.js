a542e2e1fbcf82a697bdbaf6d7889c345120d762
Quốc Khánh <bkdev98@gmail.com>
2024-08-07 10:03:29 +0700

    fix(mobile): refactor expo setup to adapt expo-updates (#214)
    Note:
    Set `EXPO_USE_METRO_WORKSPACE_ROOT=0` on EAS for production build, this variable is only necessary on development.
    
    https://github.com/expo/eas-cli/issues/2280#issuecomment-2183524390

--- apps/mobile/index.js
+++
@@ -1,6 +0,0 @@
-import '@formatjs/intl-locale/polyfill-force'
-import '@formatjs/intl-pluralrules/polyfill-force'
-import '@formatjs/intl-pluralrules/locale-data/en'
-import '@formatjs/intl-pluralrules/locale-data/vi'
-
-import 'expo-router/entry'

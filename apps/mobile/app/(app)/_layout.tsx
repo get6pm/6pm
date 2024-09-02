f9bba4b4678dc9f5fd54aed11e4c660d9ce9936f
Quốc Khánh <bkdev98@gmail.com>
2024-09-02 18:18:10 +0700

    fix(mobile): fix local auth guard cause router crashes

--- apps/mobile/app/(app)/_layout.tsx
+++ apps/mobile/app/(app)/_layout.tsx
@@ -12,6 +12,7 @@ import { useLingui } from '@lingui/react'
 import { Link, Redirect, SplashScreen, Stack } from 'expo-router'
 import { PlusIcon } from 'lucide-react-native'
 import { useEffect } from 'react'
+import { View } from 'react-native'
 
 export default function AuthenticatedLayout() {
   const { isLoaded, isSignedIn } = useUser()
@@ -35,134 +36,135 @@ export default function AuthenticatedLayout() {
     return <Redirect href={'/onboarding/step-one'} />
   }
 
-  if (shouldAuthLocal) {
-    return <AuthLocal onAuthenticated={() => setShouldAuthLocal(false)} />
-  }
-
   return (
-    <Stack
-      screenOptions={{
-        headerShown: true,
-        headerBackTitleVisible: false,
-        headerTintColor: theme[colorScheme ?? 'light'].primary,
-        headerShadowVisible: false,
-        headerTitleStyle: {
-          fontFamily: 'Inter Medium',
-          fontSize: 16,
-          color: theme[colorScheme ?? 'light'].primary,
-        },
-        headerStyle: {
-          backgroundColor: theme[colorScheme ?? 'light'].background,
-        },
-        headerLeft: () => <BackButton />,
-      }}
-    >
-      <Stack.Screen
-        name="(tabs)"
-        options={{
-          headerShown: false,
+    <View className="flex-1">
+      {shouldAuthLocal && (
+        <AuthLocal onAuthenticated={() => setShouldAuthLocal(false)} />
+      )}
+      <Stack
+        screenOptions={{
+          headerShown: true,
+          headerBackTitleVisible: false,
+          headerTintColor: theme[colorScheme ?? 'light'].primary,
+          headerShadowVisible: false,
+          headerTitleStyle: {
+            fontFamily: 'Inter Medium',
+            fontSize: 16,
+            color: theme[colorScheme ?? 'light'].primary,
+          },
+          headerStyle: {
+            backgroundColor: theme[colorScheme ?? 'light'].background,
+          },
+          headerLeft: () => <BackButton />,
         }}
-      />
-      <Stack.Screen
-        name="transaction/new-record"
-        options={{
-          presentation: 'modal',
-          headerTitle: '',
-          // headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="review-transactions"
-        options={{
-          headerTitle: t(i18n)`Review transactions`,
-        }}
-      />
-      <Stack.Screen
-        name="transaction/[transactionId]"
-        options={{
-          presentation: 'modal',
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="language"
-        options={{
-          presentation: 'modal',
-          headerTitle: t(i18n)`Language`,
-        }}
-      />
-      <Stack.Screen
-        name="profile"
-        options={{
-          presentation: 'modal',
-          headerTitle: t(i18n)`Profile`,
-        }}
-      />
-      <Stack.Screen
-        name="appearance"
-        options={{ headerTitle: t(i18n)`Appearance` }}
-      />
-      <Stack.Screen
-        name="wallet/accounts"
-        options={{
-          headerTitle: t(i18n)`Wallet accounts`,
-          headerRight: () => (
-            <Link href="/wallet/new-account" asChild>
-              <Button size="icon" variant="ghost">
-                <PlusIcon className="size-6 text-primary" />
-              </Button>
-            </Link>
-          ),
-        }}
-      />
-      <Stack.Screen
-        name="wallet/new-account"
-        options={{
-          presentation: 'modal',
-          headerTitle: t(i18n)`New account`,
-        }}
-      />
-      <Stack.Screen
-        name="wallet/[walletId]"
-        options={{
-          presentation: 'modal',
-          headerTitle: t(i18n)`Edit account`,
-        }}
-      />
-      <Stack.Screen
-        name="category/index"
-        options={{
-          headerTitle: t(i18n)`Categories`,
-          headerRight: () => (
-            <Link href="/category/new-category" asChild>
-              <Button size="icon" variant="ghost">
-                <PlusIcon className="size-6 text-primary" />
-              </Button>
-            </Link>
-          ),
-        }}
-      />
-      <Stack.Screen
-        name="category/new-category"
-        options={{
-          presentation: 'modal',
-          headerTitle: t(i18n)`New category`,
-        }}
-      />
-      <Stack.Screen
-        name="category/[categoryId]"
-        options={{
-          presentation: 'modal',
-          headerTitle: t(i18n)`Edit category`,
-        }}
-      />
-      <Stack.Screen
-        name="budget/new-budget"
-        options={{
-          presentation: 'modal',
-          headerTitle: t(i18n)`New budget`,
-        }}
-      />
-    </Stack>
+      >
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="transaction/new-record"
+          options={{
+            presentation: 'modal',
+            headerTitle: '',
+            // headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="review-transactions"
+          options={{
+            headerTitle: t(i18n)`Review transactions`,
+          }}
+        />
+        <Stack.Screen
+          name="transaction/[transactionId]"
+          options={{
+            presentation: 'modal',
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="language"
+          options={{
+            presentation: 'modal',
+            headerTitle: t(i18n)`Language`,
+          }}
+        />
+        <Stack.Screen
+          name="profile"
+          options={{
+            presentation: 'modal',
+            headerTitle: t(i18n)`Profile`,
+          }}
+        />
+        <Stack.Screen
+          name="appearance"
+          options={{ headerTitle: t(i18n)`Appearance` }}
+        />
+        <Stack.Screen
+          name="wallet/accounts"
+          options={{
+            headerTitle: t(i18n)`Wallet accounts`,
+            headerRight: () => (
+              <Link href="/wallet/new-account" asChild>
+                <Button size="icon" variant="ghost">
+                  <PlusIcon className="size-6 text-primary" />
+                </Button>
+              </Link>
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="wallet/new-account"
+          options={{
+            presentation: 'modal',
+            headerTitle: t(i18n)`New account`,
+          }}
+        />
+        <Stack.Screen
+          name="wallet/[walletId]"
+          options={{
+            presentation: 'modal',
+            headerTitle: t(i18n)`Edit account`,
+          }}
+        />
+        <Stack.Screen
+          name="category/index"
+          options={{
+            headerTitle: t(i18n)`Categories`,
+            headerRight: () => (
+              <Link href="/category/new-category" asChild>
+                <Button size="icon" variant="ghost">
+                  <PlusIcon className="size-6 text-primary" />
+                </Button>
+              </Link>
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="category/new-category"
+          options={{
+            presentation: 'modal',
+            headerTitle: t(i18n)`New category`,
+          }}
+        />
+        <Stack.Screen
+          name="category/[categoryId]"
+          options={{
+            presentation: 'modal',
+            headerTitle: t(i18n)`Edit category`,
+          }}
+        />
+        <Stack.Screen
+          name="budget/new-budget"
+          options={{
+            presentation: 'modal',
+            headerTitle: t(i18n)`New budget`,
+          }}
+        />
+      </Stack>
+    </View>
   )
 }

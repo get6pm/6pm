94966f54f04b6e95c819e67ebb1e8dd45779c18a
Dustin Do <dustin.do95@gmail.com>
2024-09-22 15:33:20 +0700

    chore(api): add test migrations

---
+++ apps/api/prisma/migrations/20240922083143_test/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "UserWalletAccount" ADD COLUMN     "foo" TEXT;
---
+++ apps/api/prisma/migrations/20240922083225_revert_test/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `foo` on the `UserWalletAccount` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "UserWalletAccount" DROP COLUMN "foo";
